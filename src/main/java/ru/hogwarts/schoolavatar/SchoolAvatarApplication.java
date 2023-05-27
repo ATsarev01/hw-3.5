cc49b319532e8cafd8afca03fc0d05291ff3ba7a
ATsarev01 <maxperov01@gmail.com>
2023-05-27 23:11:46 +0600

    initial commit

---
+++ src/main/java/ru/hogwarts/schoolavatar/SchoolAvatarApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolavatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAvatarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAvatarApplication.class, args);
+    }
+
+}
